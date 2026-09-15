5e6c5e5032b0f59691572fd6fdd4c24dc9b41fc4
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2026-09-14 20:34:01 -0400

    Add script

---
+++ getUrls.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+host="hackfest.ca"
+reject="*.mp4,*.webp,*.js,*.css,*.png,*.svg"
+outputDirectory=html
+
+url="https://hackfest.ca/"
+wget -rD $host -k -l1  --wait=0.1 --reject="$reject" --directory-prefix=$outputDirectory -- $url
+
+
+host="hfctf.ca"
+outputDirectory=html
+
+url="https://hfctf.ca/"
+wget -rD $host -k -l1 --wait=0.1 --reject="$reject" --directory-prefix=$outputDirectory -- $url
+
+
+find html -type f -exec sed -i \
+  -e 's#\(/cdn-cgi/l/email-protection\)#\1#g' \
+  -e 's#\(/cdn-cgi/l/email-protection\)#\1#g' \
+  -e 's#\(/cdn-cgi/l/email-protection\)[^"]*"#\1"#g' \
+  -e 's#data-cfemail="[^"]*"#data-cfemail=""#g' \
+  {} +
